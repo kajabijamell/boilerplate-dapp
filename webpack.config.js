92ad464c9e582e0b880b63c41c36e35bbc642a65
Jamell Jones <jjones@ripen.com>
2021-11-22 19:31:03 -0800

    fix webpack config, add open option to start

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,7 +7,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules|bower_component s)/,
         loader: "babel-loader",
         options: { presets: ["@babel/env"] }
       },
@@ -25,10 +24,7 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+    port: 3010,
+    hot: 'only'
+  }
 };
